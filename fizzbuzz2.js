6e65cf2632326bd8245deaa9a00953662a00f564
[Sean Morrell] <[seanmorrell7@gmail.com]>
2025-07-02 09:24:08 +0100

    changed indents

--- fizzbuzz2.js
+++ fizzbuzz2.js
@@ -1,15 +1,15 @@
 for (let n=1; n<=1000; n++) {
-    var txt = [];
+    	var txt = [];
 	if (n%3 == 0) {txt.push("Fizz");}
-    if (n%13 == 0) {txt.push("Fezz")}
+   	if (n%13 == 0) {txt.push("Fezz")}
 	if (n%5 == 0) {txt.push("Buzz");}
-    if (n%7 == 0) {txt.push("Bang");}
-    if (n%11 == 0) {
-        if (txt.includes("Fezz")) {txt = ["Fezz", "Bong"];}
-        else {txt = ["Bong"];}
-    }
-    if (n%17 == 0) {txt.reverse();}
+    	if (n%7 == 0) {txt.push("Bang");}
+    	if (n%11 == 0) {
+        	if (txt.includes("Fezz")) {txt = ["Fezz", "Bong"];}
+        	else {txt = ["Bong"];}
+    	}
+    	if (n%17 == 0) {txt.reverse();}
 
-    if (txt.length == 0) {console.log(n);}
-    else {console.log(txt.join());}
+    	if (txt.length == 0) {console.log(n);}
+    	else {console.log(txt.join());}
 }
